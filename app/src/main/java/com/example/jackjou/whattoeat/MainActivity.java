df6215645668df68eafd219ae16f711599f66d98
wayne841227 <u10316044@go.utaipei.edu.tw>
2017-01-02 18:06:33 +0800

    ScrollingPicker

--- app/src/main/java/com/example/jackjou/whattoeat/MainActivity.java
+++ app/src/main/java/com/example/jackjou/whattoeat/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,15 +12,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.aigestudio.wheelpicker.WheelPicker;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,WheelPicker.OnItemSelectedListener  {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
 
     private ArrayList<String> PickerData = new ArrayList<>();
+    private WheelPicker wheelPicker;
+    private Button actionBtn;
+    private int count;
+    private int position;
+    private ScheduledExecutorService scheduler;
+    private Runnable runnable;
+    private boolean isRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +61,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        WheelPicker wheelCenter = (WheelPicker) findViewById(R.id.main_wheel_center);
-        wheelCenter.setOnItemSelectedListener(this);
+//        WheelPicker wheelCenter = (WheelPicker) findViewById(R.id.main_wheel_center);
+//        wheelCenter.setOnItemSelectedListener(this);
+//
 
-        setPickerData(wheelCenter);
+        wheelPicker = (WheelPicker) findViewById(R.id.wheelPicker);
+        actionBtn = (Button) findViewById(R.id.start);
+        actionBtn.setOnClickListener(new OnActionClickListener());
+
+        isRunning = false;
+        position = 0;
+        scheduler = Executors.newScheduledThreadPool((Runtime.getRuntime().availableProcessors()));
+
+        initWheelPicker();
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (position < count) {
+                    position++;
+                } else {
+                    position = 0;
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        wheelPicker.setSelectedItemPosition(position);
+                    }
+                });
+            }
+        };
+
+        setPickerData(wheelPicker);
     }
 
 
@@ -111,24 +154,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
-    @Override
-    public void onItemSelected(WheelPicker picker, Object data, int position) {
-        //String text = "";
-        //switch (picker.getId()) {
-        //case R.id.main_wheel_left:
-        //    text = "Left:";
-        //    break;
-        //   case R.id.main_wheel_center:
-        //    text = "Center:";
-        //       break;
-        //case R.id.main_wheel_right:
-        //    text = "Right:";
-        //    break;
-        //}
-        Toast.makeText(this,  String.valueOf(data), Toast.LENGTH_SHORT).show();
-    }
+//    @Override
+//    public void onItemSelected(WheelPicker picker, Object data, int position) {
+//        //String text = "";
+//        //switch (picker.getId()) {
+//        //case R.id.main_wheel_left:
+//        //    text = "Left:";
+//        //    break;
+//        //   case R.id.main_wheel_center:
+//        //    text = "Center:";
+//        //       break;
+//        //case R.id.main_wheel_right:
+//        //    text = "Right:";
+//        //    break;
+//        //}
+//        Toast.makeText(this,  String.valueOf(data), Toast.LENGTH_SHORT).show();
+//    }
 
-    public void setPickerData(WheelPicker wheelCenter){
+    public void setPickerData(WheelPicker wheelPicker){
 
         MyDatabase db = new MyDatabase(this);
         db.openDB();
@@ -148,6 +191,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         db.closeDB();
 
-        wheelCenter.setData(PickerData);
+        wheelPicker.setData(PickerData);
+    }
+
+    private void initWheelPicker(){
+        List<String> data = new ArrayList<>();
+        data.add("Hello");
+        data.add("Hey");
+        data.add("Hi");
+        count = data.size();
+
+        wheelPicker.setData(data);
+        wheelPicker.setCyclic(true);
+        wheelPicker.setSelectedItemTextColor(ContextCompat.getColor(this, R.color.red));
+        wheelPicker.setIndicator(true);
+        wheelPicker.setSelectedItemPosition(position);
+    }
+
+    private class OnActionClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if(isRunning){
+                scheduler.shutdownNow();
+                scheduler = new ScheduledThreadPoolExecutor((Runtime.getRuntime().availableProcessors()));
+                actionBtn.setText("Start");
+            }
+            else{
+                scheduler.scheduleWithFixedDelay(runnable, 0, 40, TimeUnit.MILLISECONDS);
+                actionBtn.setText("Stop");
+            }
+            isRunning = !isRunning;
+        }
     }
 }
